7dc76d034e445192292bb4b2b567c96e5a30e3f8
carlosr301101 <carlosr301101@gmail.com>
2025-09-04 00:10:12 -0400

    arreglado alembic esta si

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from backend.api.core.models import Base  # Ajusta el import según tu estructura
+from api.core.models import Base  # Ajusta el import según tu estructura
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
